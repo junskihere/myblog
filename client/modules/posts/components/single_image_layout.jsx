dfe5dfd9844c002a2c13ff6d22503497b8c1d3bc
jun <jun@snapzio.com>
2016-06-10 16:18:49 +0800

    rasta farai

--- client/modules/posts/components/single_image_layout.jsx
+++ client/modules/posts/components/single_image_layout.jsx
@@ -11,11 +11,24 @@ class SingleImageLayout extends React.Component {
     return (
       <div >
           <div className="parallax-container">
-              <h1>Hello</h1>
+              <div className="row">
+                  <div className="col s12">
+                      <div className="col s4 green accent-4">
+                        <h1>Hello</h1>
+                      </div>
+                      <div className="col s4 red accent-4">
+                        <h1>Hello1</h1>
+                      </div>
+                      <div className="col s4 yellow accent-4">
+                        <h1>Hello2</h1>
+                      </div>
+                  </div>
+                </div>
               <div className="parallax"><img src="img/cloud-home.jpg" /></div>
           </div>
 
           {content()}
+          
       </div>
     );
   }
